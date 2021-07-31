0e79bd239985caf3aefdc3f105f22161fc3eb7b8
John Okely <jlokely@gmail.com>
2021-07-31 12:18:58 +0800

    Add webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  mode: process.env.VERCEL_ENV === "production" ? "production" : "development",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
-  mode: process.env.VERCEL_ENV === "production" ? "production" : "development",
+  plugins: [new HtmlWebpackPlugin({ title: "Ambience" })],
 };
