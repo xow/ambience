7bfe5e7446c41eb2f05db7a86e6dcd1289280a83
John Okely <jlokely@gmail.com>
2021-07-31 12:17:18 +0800

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  mode: process.env.VERCEL_ENV === "production" ? "production" : "development",
+};
