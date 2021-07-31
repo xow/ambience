a84bb359e1bcacb85590030e2433086a314a6d5e
John Okely <jlokely@gmail.com>
2021-07-31 14:43:39 +0800

    Enable typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,27 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   mode: process.env.VERCEL_ENV === "production" ? "production" : "development",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
   plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
   module: {
     rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
     ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
