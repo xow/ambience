ee86722fdfd66756ac6a04422b4119169ddc6f8d
John Okely <jlokely@gmail.com>
2021-08-07 20:10:16 +0800

    Try again

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,41 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const devMode = process.env.VERCEL_ENV !== "production";
+const devMode = process.env.VERCEL_ENV !== 'production';
 
 module.exports = (_, { mode }) => ({
-  entry: "./src/index.ts",
-  mode: devMode ? "development" : "production",
+  entry: './src/index.ts',
+  mode: devMode ? 'development' : 'production',
   plugins: [
-    ...(mode === "production" ? [new MiniCssExtractPlugin()] : []),
-    new HtmlWebpackPlugin({ template: "src/index.html" }),
+    ...(mode === 'production' ? [new MiniCssExtractPlugin()] : []),
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
   ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
         use: [
-          mode === "production" ? MiniCssExtractPlugin.loader : "style-loader",
-          "css-loader",
+          mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
         ],
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: [path.join(__dirname, "dist"), path.join(__dirname, "public")],
+    contentBase: [path.join(__dirname, 'dist'), path.join(__dirname, 'public')],
     compress: true,
     port: 3000,
     open: true,
