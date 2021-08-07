4169426335488a8e242736b11059bfeaaf0e3b19
John Okely <jlokely@gmail.com>
2021-08-07 11:00:39 +0800

    Fix Dry/wet control

--- src/AudioEffects/DryWet.ts
+++ src/AudioEffects/DryWet.ts
@@ -12,7 +12,7 @@ export function createDryWet(
   wetness: number,
 ): AudioIO {
   // Ensure wetness is between 0 and 1
-  const cleanWetnessValue = Math.min(Math.max(wetness, 1), 0);
+  const cleanWetnessValue = Math.min(Math.max(wetness, 0), 1);
 
   const input = context.createGain(); // An audio node made to route into dry & wet
 
@@ -23,12 +23,12 @@ export function createDryWet(
   dryGain.gain.value = 1 - cleanWetnessValue;
 
   input.connect(dryGain);
-  input.connect(wet); // This is the actual effect
   wet.connect(wetGain); // Connect it to a gain node so we can effect the volume
+  input.connect(wet); // This is the actual effect
 
   const output = context.createGain();
   dryGain.connect(output);
-  wet.connect(output); // TODO should be wetgain
+  wetGain.connect(output);
 
   return { input, output };
 }
