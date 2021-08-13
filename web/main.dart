e3a8be904a8ca6986e52b3b139aa1fbc67567b16
John Okely <jlokely@gmail.com>
2021-08-13 22:01:57 +0800

    Get sound coming out

--- web/main.dart
+++ web/main.dart
@@ -3,10 +3,6 @@ import 'dart:web_audio';
 import 'Instruments/Synth.dart';
 import 'Tools/Midi.dart';
 
-void onClickKey({required String note, required int octave}) {
-  window.alert(note);
-}
-
 void main() {
   /// Main audio context
   final context = AudioContext();
@@ -16,13 +12,16 @@ void main() {
 
   final playTone = getPlayTone(context: context, instrumentNode: instrument);
 
+  if (context.destination != null) {
+    instrument.connectNode(context.destination!);
+  }
+
   noteStrings.forEach((String note) {
     final key = DivElement();
     key.className = 'key';
     querySelector('.keyboard')?.children.add(key);
     key.onClick.listen((event) {
       playTone(message: 96, velocity: 127);
-      onClickKey(note: note, octave: 4);
     });
   });
 }
