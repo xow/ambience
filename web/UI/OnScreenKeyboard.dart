23c303386ca71f2c5b4d66e2e70f8ed9bccd60f5
John Okely <jlokely@gmail.com>
2021-08-14 09:08:35 +0800

    Keyboard tweaks

--- web/UI/OnScreenKeyboard.dart
+++ web/UI/OnScreenKeyboard.dart
@@ -4,12 +4,15 @@ import 'dart:async';
 import '../Tools/Midi.dart';
 
 void render(HandleMidi handleMidi) {
+  var i = 0;
   noteStrings.forEach((String note) {
+    i += 1;
     final key = DivElement();
     key.className = 'key';
     querySelector('.keyboard')?.children.add(key);
     key.onClick.listen((event) {
-      final message = octaveAndNoteToMessage(note: note, octave: 4);
+      final message =
+          octaveAndNoteToMessage(note: note, octave: (i / 12).floor() + 4);
       final stopTone =
           handleMidi(command: Commands.NOTE_ON, message: message, value: 127);
 
