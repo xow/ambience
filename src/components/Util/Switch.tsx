b7a3f4ad3aab4cad51d99e43c32d9228fed4d57f
John Okely <jlokely@gmail.com>
2021-08-24 08:00:52 +0800

    Improve switch styles

--- src/components/Util/Switch.tsx
+++ src/components/Util/Switch.tsx
@@ -8,7 +8,7 @@ function Switch({ label, value, onChange }: IProps) {
   return (
     <div>
       <label className="font-bold text-gray-700 mb-2">{label}</label>
-      <div className="flex items-center justify-center ">
+      <div className="flex mt-2">
         <label className="relative w-12 h-6 flex select-none cursor-pointer">
           <input
             type="checkbox"
@@ -19,12 +19,12 @@ function Switch({ label, value, onChange }: IProps) {
             value={value ? 'on' : 'off'}
           />
           <span
-            className={`absolute left-0 top-0 h-full w-full rounded-full ${
+            className={`shadow-md absolute left-0 top-0 h-full w-full rounded-full transition-right duration-300 ease-in-out ${
               value ? 'bg-green-500' : 'bg-gray-500'
             } `}
           ></span>
           <span
-            className={`h-6 w-6 border-2 absolute z-10 rounded-full bg-white transition-right duration-300 ease-in-out flex justify-center items-center  ${
+            className={`shadow-md h-6 w-6 border-2 absolute z-10 rounded-full bg-white transition-right duration-300 ease-in-out flex justify-center items-center  ${
               value ? 'border-green-500 left-6' : 'border-gray-500 left-0'
             }`}
           ></span>
