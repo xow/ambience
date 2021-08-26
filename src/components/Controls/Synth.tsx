cbb5ad20fb11d2e65d17fd9f76d9eba0ef8c834d
John Okely <jlokely@gmail.com>
2021-08-26 22:29:27 +0800

    #24 Synth envelope

--- src/components/Controls/Synth.tsx
+++ src/components/Controls/Synth.tsx
@@ -3,27 +3,94 @@ import { IDawSettings } from '../..';
 import { SynthParametersContext } from '../../pages';
 import PluginControl from './PluginControl';
 import Select from '../Util/Select';
+import Radial from '../Util/Radial';
 
 function Synth() {
   const { dawSettings, setDawSettings } = useContext(SynthParametersContext);
   return (
     <PluginControl pluginName="Oscillator">
-      <Select<IDawSettings['synth']['type']>
-        label="Waveform"
-        options={{
-          sawtooth: 'Saw',
-          sine: 'Sine',
-          square: 'Square',
-          triangle: 'Triangle',
-        }}
-        value={dawSettings.synth.type}
-        onChange={value =>
-          setDawSettings({
-            ...dawSettings,
-            synth: { ...dawSettings.synth, type: value },
-          })
-        }
-      />
+      <div className="flex-grow w-36">
+        <Select<IDawSettings['synth']['type']>
+          label="Waveform"
+          options={{
+            sawtooth: 'Saw',
+            sine: 'Sine',
+            square: 'Square',
+            triangle: 'Triangle',
+          }}
+          value={dawSettings.synth.type}
+          onChange={value =>
+            setDawSettings({
+              ...dawSettings,
+              synth: { ...dawSettings.synth, type: value },
+            })
+          }
+        />
+      </div>
+      <div className="flex-grow w-36">
+        <Radial
+          label="Attack"
+          value={dawSettings.synth.attack ** (1 / 3)}
+          onChange={value =>
+            setDawSettings({
+              ...dawSettings,
+              synth: { ...dawSettings.synth, attack: value ** 3 },
+            })
+          }
+          min={0}
+          max={5 ** (1 / 3)}
+          displayFunction={x => `${(x ** 3).toFixed(2)}s`}
+          decimalPlaces={10}
+        />
+      </div>
+      <div className="flex-grow w-36">
+        <Radial
+          label="Delay"
+          value={dawSettings.synth.delay ** (1 / 3)}
+          onChange={value =>
+            setDawSettings({
+              ...dawSettings,
+              synth: { ...dawSettings.synth, delay: value ** 3 },
+            })
+          }
+          min={0}
+          max={5 ** (1 / 3)}
+          displayFunction={x => `${(x ** 3).toFixed(2)}s`}
+          decimalPlaces={10}
+        />
+      </div>
+      <div className="flex-grow w-36">
+        <Radial
+          label="Sustain"
+          value={dawSettings.synth.sustain}
+          onChange={value =>
+            setDawSettings({
+              ...dawSettings,
+              synth: { ...dawSettings.synth, sustain: value ** 3 },
+            })
+          }
+          min={0}
+          max={1}
+          displayFunction={x => `${(x * 100).toFixed(1)}%`}
+          decimalPlaces={5}
+        />
+      </div>
+      <div className="flex-grow w-36">
+        <Radial
+          label="Release"
+          value={dawSettings.synth.release ** (1 / 3)}
+          onChange={value =>
+            setDawSettings({
+              ...dawSettings,
+              synth: { ...dawSettings.synth, release: value ** 3 },
+            })
+          }
+          min={0}
+          max={5 ** (1 / 3)}
+          displayFunction={x => `${(x ** 3).toFixed(2)}s`}
+          decimalPlaces={10}
+        />
+      </div>
     </PluginControl>
   );
 }
